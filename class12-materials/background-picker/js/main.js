2d3e7ecfb4731c46cc8fd6252250a8ee02f2cbbe
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-06-23 00:59:02 +0100

    delete/repeat
    deleting and repeating 2 of the exercises

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -1,20 +1,76 @@
-document.getElementById("blue").onclick = partyBlue;
+let bodyID = document.querySelector("body").style;
+document.getElementById("purple").onclick = runPurple;
+document.getElementById("green").onclick = runGreen;
+document.getElementById("blue").onclick = runBlue;
 
-function partyBlue() {
-  document.querySelector("body").style.backgroundColor = "blue";
-  document.querySelector("body").style.color = "yellow";
+function runPurple() {
+  bodyID.backgroundColor = "purple";
+  bodyID.color = "white";
 }
 
-document.getElementById("green").onclick = partyGreen;
-
-function partyGreen() {
-  document.querySelector("body").style.backgroundColor = "green";
-  document.querySelector("body").style.color = "magenta";
+function runGreen() {
+  bodyID.backgroundColor = "green";
+  bodyID.color = "white";
 }
 
-document.getElementById("purple").onclick = partyPurple;
-
-function partyPurple() {
-  document.querySelector("body").style.backgroundColor = "purple";
-  document.querySelector("body").style.color = "gold";
+function runBlue() {
+  bodyID.backgroundColor = "blue";
+  bodyID.color = "white";
 }
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// document.getElementById("blue").onclick = partyBlue;
+
+// function partyBlue() {
+//   document.querySelector("body").style.backgroundColor = "blue";
+//   document.querySelector("body").style.color = "yellow";
+// }
+
+// document.getElementById("green").onclick = partyGreen;
+
+// function partyGreen() {
+//   document.querySelector("body").style.backgroundColor = "green";
+//   document.querySelector("body").style.color = "magenta";
+// }
+
+// document.getElementById("purple").onclick = partyPurple;
+
+// function partyPurple() {
+//   document.querySelector("body").style.backgroundColor = "purple";
+//   document.querySelector("body").style.color = "gold";
+// }
--- class14-materials/class-weekend-boring/js/main.js
+++ class14-materials/class-weekend-boring/js/main.js
@@ -1,22 +1,72 @@
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
-document.querySelector("#check").addEventListener("click", runDays);
+document.querySelector("#check").addEventListener("click", dayCheck);
 
-function runDays() {
-  let compare = document.querySelector("#placeToCheck").value.toLowerCase();
+function dayCheck() {
+  console.log("testing");
+  let day = document.querySelector("#placeToCheck").value;
+  day = day.toLowerCase();
+  console.log(day);
   let placeToSee = document.querySelector("#placeToSee");
-
-  if (compare === "sunday" || compare === "saturday") {
+  if (day === "saturday" || day === "sunday") {
     placeToSee.innerHTML = "It's the weekend!";
-  } else if (compare === "tuesday" || compare === "thursday") {
-    placeToSee.innerHTML = "It's a class day!";
-  } else if (
-    compare === "monday" ||
-    compare === "wednesday" ||
-    compare === "friday"
-  ) {
+  } else if (day === "tuesday" || day === "thursday") {
+    placeToSee.innerHTML = "It's a class day";
+  } else if (day === "monday" || day === "wednesday" || day === "friday") {
     placeToSee.innerHTML = "It's a boring day";
   } else {
-    placeToSee.innerHTML = "That's not a day fam!";
+    placeToSee.innerHTML = "That's not a day dude!";
   }
 }
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// document.querySelector("#check").addEventListener("click", runDays);
+
+// function runDays() {
+//   let compare = document.querySelector("#placeToCheck").value.toLowerCase();
+//   let placeToSee = document.querySelector("#placeToSee");
+//   if (compare === "sunday" || compare === "saturday") {
+//     placeToSee.innerHTML = "It's the weekend!";
+//   } else if (compare === "tuesday" || compare === "thursday") {
+//     placeToSee.innerHTML = "It's a class day!";
+//   } else if (
+//     compare === "monday" ||
+//     compare === "wednesday" ||
+//     compare === "friday"
+//   ) {
+//     placeToSee.innerHTML = "It's a boring day";
+//   } else {
+//     placeToSee.innerHTML = "That's not a day fam!";
+//   }
+// }
